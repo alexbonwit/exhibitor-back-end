03a782561018c2a0ae6da5109a96855f6cd6bead
Alex Bonwit <alexbonwit@mac.com>
2020-03-03 10:27:30 -0600

    added methods for exhibits and museum controllers

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,3 +1,22 @@
 class ExhibitsController < ApplicationController
-    
+    def index
+        exhibits = Exhibit.all
+        render json: exhibits, include :artists :museum
+    end
+
+    def show
+        exhibit = Exhibit.find_by(id: params[:id])
+        render json: exhibit.artists, include: :museum
+    end
+
+    def create
+        exhibit = Exhibit.create(name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date], cost: params[:cost], interest_count: params[:interest_count], museum_id: params[:museum_id])
+        render json: exhibit, include: :museum
+    end
+
+    def update
+        exhibit = Exhibit.find_by(id: params[:id])
+        exhibit.update(interest_count: params[:interest_count])
+        render json: exhibit
+    end
 end
--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,2 +1,11 @@
 class MuseumsController < ApplicationController
+    def index
+        museums = Museum.all
+        render json: museums, include :exhibits
+    end
+
+    def show
+        museum = Museum.find_by(id: params[:id])
+        render json: museum.exhibits, include: :artists
+    end
 end
